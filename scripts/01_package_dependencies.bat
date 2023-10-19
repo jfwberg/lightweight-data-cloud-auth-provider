809d7fea72d43c0ff77239c1a40df97e97b6c4ee
Justus van den Berg <jfwberg@gmail.com>
2023-10-19 14:02:48 +0100

    Cleaned up dependency script

--- scripts/01_package_dependencies.bat
+++ scripts/01_package_dependencies.bat
@@ -1,14 +1,24 @@
+rem --------------------------
+rem -    MANAGED PACKAGES    -
+rem --------------------------
 rem Lightweight - Apex Unit Test Util v2@2.2.0-2
 sf package install -p "04tP30000007Ez7IAE" -w 30
-
 
 rem Lightweight - REST Util@0.9.0-1
 sf package install -p "04tP30000007FOvIAM" -w 30
 
+rem Lightweight - Auth Provider Util v2@0.5.0-1
+sf package install -p "04tP30000007GUfIAM" -w 30
 
 
+rem ---------------------------
+rem -    UNLOCKED PACKAGES    -
+rem ---------------------------
 rem Lightweight - Apex Unit Test Util v2 (Unlocked)@2.2.0-2
 sf package install -p "04tP30000007F3xIAE" -w 30
 
 rem Lightweight - REST Util (Unlocked)@0.9.0-1
 sf package install -p "04tP30000007FVNIA2" -w 30
+
+rem Lightweight - Auth Provider Util v2 (Unlocked)@0.5.0-1
+sf package install -p "04tP30000007GXtIAM" -w 30
